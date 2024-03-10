2631f945dcac003f7d0ab179c4f2b0a7a1fdde11
varun-doode <90227231+varun-doode@users.noreply.github.com>
2024-03-10 19:24:10 +0530

    Update main.tf

--- terra/main.tf
+++ terra/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region     = "us-east-2"
+  }
 
+resource "aws_instance" "example" {
+  ami           = "ami-0f5daaa3a7fb3378b" 
+  instance_type = "t2.micro"  
+
+  tags = {
+    Name = "assignment_instance"
+  }
+}
